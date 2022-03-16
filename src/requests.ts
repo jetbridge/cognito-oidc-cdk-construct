9524622d8718d2375bed9d0dae739b62b6af2607
Mischa Spiegelmock <me@mish.dev>
2022-03-16 14:11:41 +0000

    WIP

--- src/requests.ts
+++ src/requests.ts
@@ -1,34 +1,32 @@
-import got from "got"
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import fetch from 'node-fetch';
 
 export const parseResponse = (resolve: any, reject: any) => (response: any) => {
   if (response.body) {
-    resolve(JSON.parse(response.body))
+    resolve(JSON.parse(response.body));
   } else {
-    reject({ reason: "response doesn't have a body" })
+    reject({ reason: "response doesn't have a body" });
   }
-}
+};
 
 export const get = (url: string, accessToken: string) =>
   new Promise((resolve, reject) => {
-    got
-      .get(url, {
-        headers: {
-          Accept: "application/json",
-          Authorization: `token ${accessToken}`,
-        },
-      })
-      .then(parseResponse(resolve, reject))
-  })
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `token ${accessToken}`,
+      },
+    }).then(parseResponse(resolve, reject));
+  });
 
 export const post = (url: string, data?: any) =>
   new Promise((resolve, reject) => {
-    got
-      .post(url, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        ...(data ? { json: data } : {}),
-      })
-      .then(parseResponse(resolve, reject))
-  })
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      ...(data ? { json: data } : {}),
+    }).then(parseResponse(resolve, reject));
+  });
--- src/secrets.ts
+++ src/secrets.ts
@@ -1,13 +1,13 @@
-import { Secret, SecretProps } from "aws-cdk-lib/aws-secretsmanager";
-import { Construct } from "constructs";
+import { Secret, SecretProps } from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
 
 export const OIDC_SECRETS = {
-  jwtRS256Key: "JWT_KEY", // private key for signing ID Token for OIDC
-  jwtRS256KeyPub: "JWT_KEY_PUB", // public key for signing ID Token for OIDC
-  githubClientId: "GITHUB_CLIENT_ID", // the client ID for Github OAuth
-  githubClientSecret: "GITHUB_CLIENT_SECRET", // the client secret for Github OAuth
-  linkedinClientId: "LINKEDIN_CLIENT_ID", // the client ID for Linkedin OAuth
-  linkedinClientSecret: "LINKEDIN_CLIENT_SECRET", // the client secret for Linkedin OAuth
+  jwtRS256Key: 'JWT_KEY', // private key for signing ID Token for OIDC
+  jwtRS256KeyPub: 'JWT_KEY_PUB', // public key for signing ID Token for OIDC
+  githubClientId: 'GITHUB_CLIENT_ID', // the client ID for Github OAuth
+  githubClientSecret: 'GITHUB_CLIENT_SECRET', // the client secret for Github OAuth
+  linkedinClientId: 'LINKEDIN_CLIENT_ID', // the client ID for Linkedin OAuth
+  linkedinClientSecret: 'LINKEDIN_CLIENT_SECRET', // the client secret for Linkedin OAuth
 };
 
 export interface SecretsOidcProps extends SecretProps {}
@@ -20,16 +20,16 @@ export class SecretsOidc extends Secret {
       // secret default template
       generateSecretString: {
         secretStringTemplate: JSON.stringify({
-          [OIDC_SECRETS.jwtRS256Key]: "replace me",
-          [OIDC_SECRETS.jwtRS256KeyPub]: "replace me",
-          [OIDC_SECRETS.githubClientId]: "replace me",
-          [OIDC_SECRETS.githubClientSecret]: "replace me",
-          [OIDC_SECRETS.linkedinClientId]: "replace me",
-          [OIDC_SECRETS.linkedinClientSecret]: "replace me",
+          [OIDC_SECRETS.jwtRS256Key]: 'replace me',
+          [OIDC_SECRETS.jwtRS256KeyPub]: 'replace me',
+          [OIDC_SECRETS.githubClientId]: 'replace me',
+          [OIDC_SECRETS.githubClientSecret]: 'replace me',
+          [OIDC_SECRETS.linkedinClientId]: 'replace me',
+          [OIDC_SECRETS.linkedinClientSecret]: 'replace me',
         }),
         // unused
         // but we need to generate something or it complains
-        generateStringKey: "RANDOM",
+        generateStringKey: 'RANDOM',
       },
     });
   }
